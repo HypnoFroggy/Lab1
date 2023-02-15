e850dc8159670e56df48eaf8ae345c99d63ebd5c
Mitya <dmitrij.mal.tim.04@gmail.com>
2023-02-15 18:28:39 +0300

    go dz1

---
+++ dz1.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = b * b - 4 * a * c
+
+if d < 0:
+    print("No roots(")
+elif d == 0:
+    x = ((-1)*b) / (2*a)
+    print("x = ", x)
+else:
+    x1 = ((-1)*b + pow(d, 0.5)) / (2*a)
+    x2 = ((-1)*b - pow(d, 0.5)) / (2*a)
+    print("x1 = ", x1)
+    print("x2 = ", x2)
